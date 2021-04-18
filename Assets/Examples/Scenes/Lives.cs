e081dfd17ce72e745f6f836b2f42753d172fe836
C10ke <78232488+C10ke@users.noreply.github.com>
2021-04-17 21:07:29 -0500

    2AgentsScence
    All working

---
+++ Assets/Examples/Scenes/Lives.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Lives : MonoBehaviour
+{
+    
+    private int lives = 3;    
+    public Text livesText;
+
+    private void OnTriggerEnter(Collider other)
+    {        
+
+        if (other.gameObject.tag == "Player")
+        {
+            Debug.Log("player hit");
+            lives -= 1;
+            livesText.text = "Lives: " + lives.ToString();
+            if (lives == 0)
+            {
+                SceneManager.LoadScene("1_multiple_agent_sizes");
+            }
+
+        }
+
+    }
+    
+
+}
---
+++ Assets/Examples/Scenes/Prize.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Prize : MonoBehaviour
+{
+    private int score = 0;
+    
+    public Text scoreText;
+    
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("got prize");
+        if (other.gameObject.tag == "Prize")
+        {
+            Debug.Log("got prize");
+            other.gameObject.SetActive(false);
+            score += 10;
+            scoreText.text = "Score: " + score.ToString();
+        }
+
+       
+
+    }
+        
+
+}
